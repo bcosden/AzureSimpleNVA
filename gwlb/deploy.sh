c1413f9a6c5688bfa2248899383c9e7e57122f18
Bruce M. Cosden <bruce.cosden@gmail.com>
2022-06-19 11:12:47 -0400

    updated

--- gwlb/deploy.sh
+++ gwlb/deploy.sh
@@ -33,17 +33,25 @@ az network vnet create --address-prefixes 10.1.0.0/16 -n nvaVnet -g $rg --subnet
 echo -e "$WHITE[$(date +"%T")]$GREEN Creating App Virtual Network$WHITE"
 az network vnet create --address-prefixes 192.168.0.0/16 -n appVnet -g $rg --subnet-name app --subnet-prefixes 192.168.0.0/24 -o none
 
-# create Application VM
+# create NSG for NVA VM and Application VM
 mypip=$(curl -4 ifconfig.io -s)
-echo -e "$WHITE[$(date +"%T")]$GREEN Create Public IP, NSG, and Allow SSH on port 22 for IP: $WHITE"$mypip
-az network nsg create -g $rg -n $vmapp"NSG" -o none
-az network nsg rule create -n "Allow-SSH" --nsg-name $vmapp"NSG" --priority 300 -g $rg --direction Inbound --protocol TCP --source-address-prefixes $mypip --destination-port-ranges 22 -o none
+echo -e "$WHITE[$(date +"%T")]$GREEN Create NSG and Allow SSH on port 22 for IP: $WHITE"$mypip
+az network nsg create -g $rg -n "serverNSG" -o none
+az network nsg rule create -n "Allow-SSH" --nsg-name "serverNSG" --priority 500 -g $rg --direction Inbound --protocol TCP --source-address-prefixes $mypip --destination-port-ranges 22 -o none
+az network nsg rule create -n web8080 --nsg-name "serverNSG" -g $rg --priority 510 --destination-port-ranges 8080 --access Allow --protocol Tcp -o none
+az network nsg rule create -n https --nsg-name "serverNSG" -g $rg --priority 520 --destination-port-ranges 443 --access Allow --protocol Tcp -o none
+az network nsg rule create -n web80 --nsg-name "serverNSG" -g $rg --priority 530 --destination-port-ranges 80 --access Allow --protocol Tcp -o none
+az network nsg rule create -n vxlan --nsg-name "serverNSG" -g $rg --priority 540 --destination-port-ranges 4789 --access Allow --protocol Udp -o none
+az network nsg rule create -n tunnels --nsg-name "serverNSG" -g $rg --priority 550 --destination-port-ranges 10800-10801 --access Allow --protocol Udp -o none
+
+# create Application VM
+echo -e "$WHITE[$(date +"%T")]$GREEN Create Application Public IP and NIC $WHITE"
 az network public-ip create -g $rg -n $vmapp"-pip" --sku standard --allocation-method static -o none --only-show-errors
-az network nic create -g $rg --vnet-name appVnet --subnet app -n $vmapp"NIC" --public-ip-address $vmapp"-pip" --network-security-group $vmapp"NSG" -o none
+az network nic create -g $rg --vnet-name appVnet --subnet app -n $vmapp"NIC" --public-ip-address $vmapp"-pip" --network-security-group "serverNSG" -o none
 
 # default is to use your local .ssh key in folder ~/.ssh/id_rsa.pub
 if [ $usessh == "true" ]; then
-    echo -e "$WHITE[$(date +"%T")]$GREEN Creating Application VM using ssh $WHITE"
+    echo -e "$WHITE[$(date +"%T")]$GREEN Creating Application VM using public key $WHITE"
     az vm create -n $vmapp -g $rg \
         --image ubuntults \
         --size $vmsize \
@@ -93,9 +101,13 @@ appvmconfig=$(az network nic show --ids $appvmnicid --query 'ipConfigurations[0]
 az network nic ip-config address-pool add --nic-name $vmapp"NIC" -g $rg --ip-config-name $appvmconfig --lb-name applb --address-pool vms -o none
 
 # test health ok
-#echo -e '$WHITE['$(date +"%T")']$GREEN Test Load Balancer Health'
-#applbpip=$(az network public-ip show -n applb-pip -g $rg --query ipAddress -o tsv)
-#curl "http://${applbpip}:8080/api/healthcheck"
+echo -e "$WHITE[$(date +"%T")]$GREEN Test Application Load Balancer Health $WHITE"
+applbpip=$(az network public-ip show -n applb-pip -g $rg --query ipAddress -o tsv)
+apppubip=$(az network public-ip show -n $vmapp"-pip" -g $rg --query ipAddress -o tsv)
+echo "app public ip health check:"
+curl "http://${apppubip}:8080/api/healthcheck"
+echo "lb health check:"
+curl "http://${applbpip}:8080/api/healthcheck"
 
 # create GWLB
 echo -e "$WHITE[$(date +"%T")]$GREEN Creating Gateway Load Balancer$WHITE"
@@ -120,16 +132,13 @@ applbpip=$(az network public-ip show -n applb-pip -g $rg --query ipAddress -o ts
 sed 's/GWLB_PIP/'$gwlbpip'/g;s/APPLB_PIP/'$applbpip'/g' cloud-nvainit > cloud-nvainit.tmp
 
 # create NVA VM
-mypip=$(curl -4 ifconfig.io -s)
-echo -e "$WHITE[$(date +"%T")]$GREEN Create Public IP, NSG, and Allow SSH on port 22 for IP: $WHITE"$mypip
-az network nsg create -g $rg -n $vmnva"NSG" -o none
-az network nsg rule create -n "Allow-SSH" --nsg-name $vmnva"NSG" --priority 300 -g $rg --direction Inbound --protocol TCP --source-address-prefixes $mypip --destination-port-ranges 22 -o none
+echo -e "$WHITE[$(date +"%T")]$GREEN Create NVA Public IP and NIC $WHITE"
 az network public-ip create -g $rg -n $vmnva"-pip" --sku standard --allocation-method static -o none --only-show-errors
-az network nic create -g $rg --vnet-name nvaVnet --subnet nva -n $vmnva"NIC" --public-ip-address $vmnva"-pip" --network-security-group $vmnva"NSG" --ip-forwarding -o none
+az network nic create -g $rg --vnet-name nvaVnet --subnet nva -n $vmnva"NIC" --public-ip-address $vmnva"-pip" --network-security-group "serverNSG" --ip-forwarding -o none
 
 # default is to use your local .ssh key in folder ~/.ssh/id_rsa.pub
 if [ $usessh == "true" ]; then
-    echo -e "$WHITE[$(date +"%T")]$GREEN Creating NVA VM using ssh $WHITE"
+    echo -e "$WHITE[$(date +"%T")]$GREEN Creating NVA VM using public key $WHITE"
     az vm create -n $vmnva -g $rg \
         --image ubuntults \
         --size $vmsize \
@@ -164,4 +173,20 @@ az network nic ip-config address-pool add --nic-name $vmnva"NIC" -g $rg --ip-con
 echo -e "$WHITE[$(date +"%T")]$GREEN Chain App Load Balancer to Gateway Load Balancer$WHITE"
 gwlbid=$(az network lb frontend-ip show --lb-name gwlb -g $rg -n nvafrontend --query id -o tsv)
 az network lb frontend-ip update -n vmfrontend --lb-name applb -g $rg --public-ip-address applb-pip --gateway-lb $gwlbid -o none --only-show-errors
+
+# test health ok after deployment of GWLB
+echo -e "$WHITE[$(date +"%T")]$GREEN Test Application Load Balancer Health After GWLB Config $WHITE"
+echo "lb health check:"
+curl "http://${applbpip}:8080/api/healthcheck"
+
+# output key variables
+echo -e "$WHITE[$(date +"%T")]$GREEN Deployment Complete: $WHITE"
+echo "front end app load balancer ip: "$applbpip
+nvapubip=$(az network public-ip show -n $vmnva"-pip" -g $rg --query ipAddress -o tsv)
+echo "nva public ip: "$nvapubip
+echo ""
+echo "To check deployment: curl http://"$applbpip":8080/api/ip"
+echo ""
+echo "Result of Application API call:"
+curl "http://${applbpip}:8080/api/ip"
 
