f9b9cb01810c8a6c711a40ee165c646cbf70bc9b
Bruce M. Cosden <bruce.cosden@gmail.com>
2022-06-20 17:20:30 -0400

    update

--- 1nic/deploy.sh
+++ 1nic/deploy.sh
@@ -4,6 +4,16 @@
 rg="rsQuagga-1nic"
 loc="eastus"
 
+BLACK="\033[30m"
+RED="\033[31m"
+GREEN="\033[32m"
+YELLOW="\033[33m"
+BLUE="\033[34m"
+PINK="\033[35m"
+CYAN="\033[36m"
+WHITE="\033[37m"
+NORMAL="\033[0;39m"
+
 vmname="QuaggaVM"
 vmspoke1="spoke1VM"
 vmspoke2="spoke2VM"
@@ -13,15 +23,15 @@ password="MyP@ssword123"
 vmsize="Standard_D2S_v3"
 
 # create a resource group
-echo '['$(date +"%T")'] Creating Resource Group' $rg 'in' $loc
+echo -e "$WHITE[$(date +"%T")]$GREEN Creating Resource Group$CYAN" $rg"$GREEN in $CYAN"$loc"$WHITE"
 az group create -n $rg -l $loc -o none
 
 # create nva virtual network
-echo '['$(date +"%T")'] Creating Virtual Network hubVnet'
+echo -e "$WHITE[$(date +"%T")]$GREEN Creating Virtual Network hubVnet $WHITE"
 az network vnet create --address-prefixes 10.1.0.0/16 -n hubVnet -g $rg --subnet-name RouteServerSubnet --subnet-prefixes 10.1.1.0/25 -o none
 
 # create nva subnets
-echo '['$(date +"%T")'] Creating subnets'
+echo -e "$WHITE[$(date +"%T")]$GREEN Creating subnets $WHITE"
 echo ".... creating subnet1"
 az network vnet subnet create -g $rg --vnet-name hubVnet -n subnet1 --address-prefixes 10.1.2.0/24 -o none
 echo ".... creating subnet2"
@@ -34,38 +44,44 @@ echo ".... creating AzureBastionSubnet"
 az network vnet subnet create -g $rg --vnet-name hubVnet -n AzureBastionSubnet --address-prefixes 10.1.6.0/26 -o none
 
 # create spoke virtual networks
-echo '['$(date +"%T")'] Creating Virtual Network spoke1Vnet'
+echo -e "$WHITE[$(date +"%T")]$GREEN Creating Virtual Network spoke1Vnet $WHITE"
 az network vnet create --address-prefixes 10.10.0.0/16 -n spoke1Vnet -g $rg --subnet-name app --subnet-prefixes 10.10.0.0/24 -o none
 
 # create spoke virtual networks
-echo '['$(date +"%T")'] Creating Virtual Network spoke2Vnet'
+echo -e "$WHITE[$(date +"%T")]$GREEN Creating Virtual Network spoke2Vnet $WHITE"
 az network vnet create --address-prefixes 10.11.0.0/16 -n spoke2Vnet -g $rg --subnet-name app --subnet-prefixes 10.11.0.0/24 -o none
 
 # create spoke virtual networks
-echo '['$(date +"%T")'] Creating Virtual Network spoke3Vnet'
+echo -e "$WHITE[$(date +"%T")]$GREEN Creating Virtual Network spoke3Vnet $WHITE"
 az network vnet create --address-prefixes 10.12.0.0/16 -n spoke3Vnet -g $rg --subnet-name app --subnet-prefixes 10.12.0.0/24 -o none
 
 # peer virtual networks (spoke to hub)
+echo -e "$WHITE[$(date +"%T")]$GREEN Peer hub to spokes $WHITE"
 hubid=$(az network vnet show -g $rg -n hubVnet --query id -o tsv)
 spoke1id=$(az network vnet show -g $rg -n spoke1Vnet --query id -o tsv)
 spoke2id=$(az network vnet show -g $rg -n spoke2Vnet --query id -o tsv)
 spoke3id=$(az network vnet show -g $rg -n spoke3Vnet --query id -o tsv)
 # peer spoke1
+echo ".... peering spoke1"
 az network vnet peering create -n "hubTOspoke1" -g $rg --vnet-name hubVnet --remote-vnet $spoke1id --allow-vnet-access --allow-forwarded-traffic --allow-gateway-transit -o none
 az network vnet peering create -n "spoke1TOhub" -g $rg --vnet-name spoke1Vnet --remote-vnet $hubid --allow-vnet-access --allow-forwarded-traffic --use-remote-gateways -o none
 # peer spoke2
+echo ".... peering spoke2"
 az network vnet peering create -n "hubTOspoke2" -g $rg --vnet-name hubVnet --remote-vnet $spoke2id --allow-vnet-access --allow-forwarded-traffic --allow-gateway-transit -o none
 az network vnet peering create -n "spoke2TOhub" -g $rg --vnet-name spoke2Vnet --remote-vnet $hubid --allow-vnet-access --allow-forwarded-traffic --use-remote-gateways -o none
 # peer spoke3
+echo ".... peering spoke3"
 az network vnet peering create -n "hubTOspoke3" -g $rg --vnet-name hubVnet --remote-vnet $spoke3id --allow-vnet-access --allow-forwarded-traffic --allow-gateway-transit -o none
 az network vnet peering create -n "spoke3TOhub" -g $rg --vnet-name spoke3Vnet --remote-vnet $hubid --allow-vnet-access --allow-forwarded-traffic --use-remote-gateways -o none
 
 # create Bastion
+echo -e "$WHITE[$(date +"%T")]$GREEN Create Bastion $WHITE"
 az network public-ip create --name bastion-pip --resource-group $rg -l $loc --sku Standard --only-show-errors -o none
 az network bastion create -g $rg -n bastion --public-ip-address bastion-pip --vnet-name hubVnet -l $loc --only-show-errors -o none
 
 # Turn on SSH tunneling
 # az cli does not have a property to enable SSH tunneling, so must be done via rest API
+echo -e "$WHITE[$(date +"%T")]$GREEN Turn on SSH Tunneling $WHITE"
 subid=$(az account show --query 'id' -o tsv)
 uri='https://management.azure.com/subscriptions/'$subid'/resourceGroups/'$rg'/providers/Microsoft.Network/bastionHosts/bastion?api-version=2021-08-01'
 json='{
@@ -94,7 +110,7 @@ az rest --method PUT \
     --output none
 
 # create route server
-echo '['$(date +"%T")'] Creating Routeserver'
+echo -e "$WHITE[$(date +"%T")]$GREEN Creating Routeserver $WHITE"
 subnet_id=$(az network vnet subnet show \
     --name RouteServerSubnet \
     --resource-group $rg \
@@ -116,6 +132,7 @@ az network routeserver create \
     --output none
 
 # create route table for Quagga VM to reach internet
+echo -e "$WHITE[$(date +"%T")]$GREEN Create Route Table for NVA to Internet $WHITE"
 az network route-table create -g $rg -n nvaroute -o none
 az network route-table route create -g $rg --route-table-name nvaroute -n tointernet \
     --next-hop-type Internet --address-prefix 0.0.0.0/0 -o none
@@ -123,36 +140,36 @@ az network vnet subnet update -g $rg -n nva --vnet-name hubVnet --route-table nv
 
 # create QuaggaVM
 mypip=$(curl -4 ifconfig.io -s)
-echo '['$(date +"%T")'] Create Public IP, NSG, and Allow SSH on port 22 for IP: '$mypip
+echo -e "$WHITE[$(date +"%T")]$GREEN Create Public IP, NSG, and Allow SSH on port 22 for IP: $WHITE"$mypip
 az network nsg create -g $rg -n $vmname"NSG" -o none
 az network nsg rule create -n "Allow-SSH" --nsg-name $vmname"NSG" --priority 300 -g $rg --direction Inbound --protocol TCP --source-address-prefixes $mypip --destination-port-ranges 22 -o none
 az network public-ip create -n $vmname"-pip" -g $rg --version IPv4 --sku Standard -o none --only-show-errors 
 
-echo '['$(date +"%T")'] Creating Quagga VM'
+echo -e "$WHITE[$(date +"%T")]$GREEN Creating Quagga VM $WHITE"
 az network nic create -g $rg --vnet-name hubVnet --subnet nva -n $vmname"NIC" --public-ip-address $vmname"-pip" --private-ip-address 10.1.4.10 --network-security-group $vmname"NSG" --ip-forwarding true -o none
 az vm create -n $vmname -g $rg --image ubuntults --size $vmsize --nics $vmname"NIC" --authentication-type ssh --admin-username $username --ssh-key-values @~/.ssh/id_rsa.pub --custom-data cloud-init -o none --only-show-errors
 
 # create Spoke1 VM
-echo '['$(date +"%T")'] Creating Spoke1 VM'
+echo -e "$WHITE[$(date +"%T")]$GREEN Creating Spoke1 VM $WHITE"
 az network nic create -g $rg --vnet-name spoke1Vnet --subnet app -n $vmspoke1"NIC" -o none
 az vm create -n $vmspoke1 -g $rg --image ubuntults --size $vmsize --nics $vmspoke1"NIC" --authentication-type ssh --admin-username $username --ssh-key-values @~/.ssh/id_rsa.pub -o none --only-show-errors
 
 # create Spoke2 VM
-echo '['$(date +"%T")'] Creating Spoke2 VM'
+echo -e "$WHITE[$(date +"%T")]$GREEN Creating Spoke2 VM $WHITE"
 az network nic create -g $rg --vnet-name spoke2Vnet --subnet app -n $vmspoke2"NIC" -o none
 az vm create -n $vmspoke2 -g $rg --image ubuntults --size $vmsize --nics $vmspoke2"NIC" --authentication-type ssh --admin-username $username --ssh-key-values @~/.ssh/id_rsa.pub -o none --only-show-errors
 
 # create Spoke3 VM
-echo '['$(date +"%T")'] Creating Spoke3 VM'
+echo -e "$WHITE[$(date +"%T")]$GREEN Creating Spoke3 VM $WHITE"
 az network nic create -g $rg --vnet-name spoke3Vnet --subnet app -n $vmspoke3"NIC" -o none
 az vm create -n $vmspoke3 -g $rg --image ubuntults --size $vmsize --nics $vmspoke3"NIC" --authentication-type ssh --admin-username $username --ssh-key-values @~/.ssh/id_rsa.pub -o none --only-show-errors
 
 # enable b2b
-echo '['$(date +"%T")'] Enable B2B on RouteServer'
+echo -e "$WHITE[$(date +"%T")]$GREEN Enable B2B on RouteServer $WHITE"
 az network routeserver update --name rshub --resource-group $rg --allow-b2b-traffic true -o none
 
 # create peering
-echo '['$(date +"%T")'] Creating RouteServer Peering'
+echo -e "$WHITE[$(date +"%T")]$GREEN Creating RouteServer Peering $WHITE"
 az network routeserver peering create \
     --name Quagga \
     --peer-ip 10.1.4.10 \
@@ -162,13 +179,13 @@ az network routeserver peering create \
     --output none
 
 # list routes
-echo '['$(date +"%T")'] Quagga deployed. Listing Advertised Routes:'
+echo -e "$WHITE[$(date +"%T")]$GREEN Quagga deployed. Listing Advertised Routes: $WHITE"
 az network routeserver peering list-advertised-routes \
     --name Quagga \
     --routeserver rshub \
     --resource-group $rg
 
-echo '['$(date +"%T")'] Listing Learned Routes:'
+echo -e "$WHITE[$(date +"%T")]$GREEN Listing Learned Routes: $WHITE"
 az network routeserver peering list-learned-routes \
     --name Quagga \
     --routeserver rshub \
